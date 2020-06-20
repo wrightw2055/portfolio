593a761667b6bc849dbbbb56f0fb6c63180f62f2
wrightw <wrightw2055@gmail.com>
2020-06-20 09:40:41 -0400

    first 2 projects

---
+++ scripts.js
@@ -0,0 +1,24 @@
+//social icon hijinks
+let iconBox = document.getElementById('icon-container');
+
+let linkedin = document.getElementById('linkedin');
+let github = document.getElementById('github');
+let generalAssembly = document.getElementById('generalAssembly');
+let email = document.getElementById('email');
+let resume = document.getElementById('resume');
+
+iconBox.addEventListener('mouseover', () => {
+	linkedin.setAttribute('src', 'images/icons/001-linkedin.png');
+	github.setAttribute('src', 'images/icons/github.png');
+	generalAssembly.setAttribute('src', 'images/icons/ga-logo.png');
+	email.setAttribute('src', '');
+	resume.setAttribute('src', 'images/icons/004-profile.png');
+});
+
+iconBox.addEventListener('mouseout', () => {
+	linkedin.setAttribute('src', 'images/icons/045-linkedin.png');
+	github.setAttribute('src', 'images/icons/038-github.png');
+	generalAssembly.setAttribute('src', 'images/icons/ga-logo-bw.png');
+    email.setAttribute('src', 'images/inbox photo.jpg');
+	resume.setAttribute('src', 'images/icons/005-profile-1.png');
+});
